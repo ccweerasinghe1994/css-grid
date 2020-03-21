e461d77092262ce3a2c276ca9bcab8adc9dceb4f
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-03-21 20:34:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.scss";
+import { Card } from "./components/card.component";
 
 function App() {
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {cards.map(data => (
+        <Card number={data} />
+      ))}
     </div>
   );
 }
